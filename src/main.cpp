3f014d79e25606f63569ae29513af16a929274e7
LZ Labs <lztogibot@gmail.com>
2026-08-10 18:58:25 -0300

    fix: simplify initial GeoBR hub popup

--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,6 @@
 
 using namespace geode::prelude;
 
-class GeoBRHub : public FLAlertLayer {
-protected:
-    bool init() {
-        if (!FLAlertLayer::init(320.f, 220.f)) {
-            return false;
-        }
-
-        this->setTitle("GeoBR Hub");
-
-        auto title = CCLabelBMFont::create("COMMUNITY HUB", "goldFont.fnt");
-        title->setScale(0.65f);
-        title->setPosition({160.f, 175.f});
-        m_mainLayer->addChild(title);
-
-        auto info = CCLabelBMFont::create(
-            "Profile   |   Ranking   |   Events\nCreator Battles   |   Badges",
-            "bigFont.fnt"
-        );
-        info->setAlignment(kCCTextAlignmentCenter);
-        info->setScale(0.42f);
-        info->setPosition({160.f, 125.f});
-        m_mainLayer->addChild(info);
-
-        auto status = CCLabelBMFont::create("GeoBR Mod 1.0.0", "chatFont.fnt");
-        status->setScale(0.65f);
-        status->setPosition({160.f, 55.f});
-        m_mainLayer->addChild(status);
-
-        return true;
-    }
-
-public:
-    static GeoBRHub* create() {
-        auto ret = new GeoBRHub();
-        if (ret && ret->init()) {
-            ret->autorelease();
-            return ret;
-        }
-        CC_SAFE_DELETE(ret);
-        return nullptr;
-    }
-};
-
 class $modify(GeoBRMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) {
@@ -81,6 +38,10 @@ class $modify(GeoBRMenuLayer, MenuLayer) {
     }
 
     void onGeoBR(CCObject*) {
-        GeoBRHub::create()->show();
+        FLAlertLayer::create(
+            "GeoBR Hub",
+            "COMMUNITY HUB\n\nProfile   |   Ranking   |   Events\nCreator Battles   |   Badges\n\nGeoBR Mod 1.0.0",
+            "OK"
+        )->show();
     }
 };
